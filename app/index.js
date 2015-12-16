c0949e1969289c215bbaed60b00f2af09c1b7f93
clabbe <clement.labbe@rea-group.com>
2015-12-16 16:00:53 +1100

    It works

---
+++ app/index.js
@@ -0,0 +1,15 @@
+// Load the http module to create an http server.
+var http = require('http');
+var fs = require('fs')
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200);
+  response.end(fs.readFileSync('hello.html'));
+});
+
+// Listen on port 8000
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server running on port 8000");
